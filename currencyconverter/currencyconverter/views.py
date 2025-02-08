ad2e448fd24fe9622411c5311ed4e020fa477ea9
Eduard Kaverinskyi <eduard.kaverinskyi@gmail.com>
2025-02-08 09:13:48 +0100

    currencyconverter/views.py: improve handling for convert()
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@gmail.com>

--- currencyconverter/currencyconverter/views.py
+++ currencyconverter/currencyconverter/views.py
@@ -1,5 +1,4 @@
 import json
-import math
 
 from django.http import JsonResponse
 from django.shortcuts import HttpResponse
@@ -21,17 +20,24 @@ def convert(request):
         value = data.get("value", "")
 
         if not currency_from or not currency_to or not value:
-            return JsonResponse(
-                {
-                    "error": "Required fields: 'from', 'to', 'value'"
-                }
-            )
+            return JsonResponse({"error": "Required fields: 'from', 'to', 'value'"})
+
+        for currency in [currency_from, currency_to]:
+            if currency_from not in conversion_table.keys():
+                return JsonResponse(
+                    {"error": f"'Currency '{currency}' is not supported'"}
+                )
+
+        value = float(value)
+        if value < 0:
+            return JsonResponse({"error": f"Cannot convert negative value: '{value}'"})
+
+        if value == 0:
+            return JsonResponse({"value": 0})
 
         rate = conversion_table[currency_from] / conversion_table[currency_to]
-        print(f"rate is {rate}")
         value = round(float(value) * rate, ndigits=2)
-        print(f"value: {value}")
-        return JsonResponse({"value": str(value)})
+        return JsonResponse({"value": value})
 
     except json.JSONDecodeError:
         return JsonResponse({"error": "Invalid JSON"}, status=400)
